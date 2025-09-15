d1bde5e1e2a0b635715f3b60e1d80b474b27c22f
theNunu <152096019+theNunu@users.noreply.github.com>
2025-09-15 10:30:49 -0500

    emergencia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Anything } from './pages/anything/anything';
+import { Champion } from './pages/champion/champion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        // title: 'App Home Page',
+        component: Anything
+    },
+    {
+        path: "champion",
+        component: Champion
+
+    },
+    {
+        path: "**",
+        redirectTo: ""
+    }
+];
